315f6cee666a8323c6734a3926c4994ad8921e16
“Zahed75” <zahedhasan.cs@gmail.com>
2023-10-17 05:03:48 +0600

    modified:   lib/Screen/ProductCreateScreen.dart 	modified:   lib/Style/style.dart 	modified:   lib/main.dart

---
+++ lib/Screen/ProductCreateScreen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+// stateful Widget
+class ProductCreateScreen extends StatefulWidget{
+  const ProductCreateScreen({super.key});
+
+  @override
+  State<ProductCreateScreen> createState()=> _ProductCreateScreenState();
+
+}
+
+class _ProductCreateScreenState extends State<ProductCreateScreen>{
+  @override
+  Widget build (BuildContext context){
+    return Scaffold(
+      appBar: AppBar(title:const Text('CreateProduct'),),
+      body:Stack(
+        children: [
+            Container(
+              child:(SingleChildScrollView(
+                padding:const EdgeInsets.all(20),
+                child:Column(
+                  children: [
+                    TextFormField(onChanged: (value){},),
+                    TextFormField(onChanged: (value){},),
+                    TextFormField(onChanged: (value){},),
+                    TextFormField(onChanged: (value){},),
+                    TextFormField(onChanged: (value){},),
+                  ],
+                )
+              ))
+            )
+        ],
+      )
+    );
+  }
+}
