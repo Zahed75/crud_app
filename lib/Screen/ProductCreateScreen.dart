d5a7e98f49f54a8365eb4817a373ee8afb3f45d5
“Zahed75” <zahedhasan.cs@gmail.com>
2023-10-20 02:13:14 +0600

    modified:   lib/Screen/ProductCreateScreen.dart 	modified:   pubspec.lock 	modified:   pubspec.yaml

--- lib/Screen/ProductCreateScreen.dart
+++ lib/Screen/ProductCreateScreen.dart
@@ -1,50 +1,64 @@
 import 'package:flutter/material.dart';
- 
 import '../Style/style.dart';
-
+import 'package:flutter_svg/flutter_svg.dart';
 // stateful Widget
 
-class ProductCreateScreen extends StatefulWidget{
+class ProductCreateScreen extends StatefulWidget {
   const ProductCreateScreen({super.key});
 
   @override
-  State<ProductCreateScreen> createState()=> _ProductCreateScreenState();
-
+  State<ProductCreateScreen> createState() => _ProductCreateScreenState();
 }
 
-class _ProductCreateScreenState extends State<ProductCreateScreen>{
+class _ProductCreateScreenState extends State<ProductCreateScreen> {
   @override
-  Widget build (BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title:const Text('CreateProduct'),),
-      body:Stack(
-        children: [
+        appBar: AppBar(
+          title: const Text('CreateProduct'),
+        ),
+        body: Stack(
+          children: [
             Container(
-              child:(SingleChildScrollView(
-                padding:const EdgeInsets.all(20),
-                child:Column(
-                  children: [
-                    TextFormField(
-                      onChanged: (value){},
-                      decoration: AppInputDecoration('ProductName'),
-                    
-                      
-                      ),
-
-
-                    TextFormField(
-                      onChanged: (value){},
-                       decoration: AppInputDecoration('ProductName'),
-                    ),
-                    TextFormField(onChanged: (value){},),
-                    TextFormField(onChanged: (value){},),
-                    TextFormField(onChanged: (value){},),
-                  ],
-                )
-              ))
-            )
-        ],
-      )
-    );
+                child: (SingleChildScrollView(
+                    padding: const EdgeInsets.all(20),
+                    child: Column(
+                      children: [
+                        TextFormField(
+                          onChanged: (value) {},
+                          decoration: AppInputDecoration('ProductName'),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        TextFormField(
+                          onChanged: (value) {},
+                          decoration: AppInputDecoration('ProductCode'),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        TextFormField(
+                          onChanged: (value) {},
+                          decoration: AppInputDecoration('ProductImage'),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        TextFormField(
+                          onChanged: (value) {},
+                          decoration: AppInputDecoration('Unit Price'),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        TextFormField(
+                          onChanged: (value) {},
+                          decoration: AppInputDecoration('Total Price'),
+                        ),
+                      ],
+                    ))))
+          ],
+        ));
   }
 }
